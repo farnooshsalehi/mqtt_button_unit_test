c865b656391f3970ec09c47a7a8589bcab4fce05
FarnooshSalehi3012 <farnooshsalehi101079@gmail.com>
2025-04-14 23:02:02 +0330

    header file added to the main code in addition to have .c file. because we need some important vars be aacessible in test folder

---
+++ main/mqtt-debounce-interr-button.h
@@ -0,0 +1,9 @@
+#ifndef MQTT_BUTTON_EXAMPLE_H
+#define MQTT_BUTTON_EXAMPLE_H
+
+extern volatile bool button_event;
+extern volatile uint32_t last_interrupt_time;
+extern uint32_t press_count;
+extern esp_mqtt_client_handle_t mqtt_client;
+
+#endif
